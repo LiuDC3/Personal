b7d482799c6285256e131302f7316edad9fe5eec
Esteban Carranza <147107081+LiuDC3@users.noreply.github.com>
2023-10-05 21:46:21 -0500

    Add files via upload

---
+++ Programación python/Variables1.py
@@ -0,0 +1,9 @@
+numero1 = float(input("Ingresa el primer número: "))
+numero2 = float(input("Ingresa el segundo número: "))
+numero3 = float(input("Ingresa el tercer número: "))
+
+
+resultado = numero1 * numero2 * numero3
+
+
+print("El resultado de la multiplicación es:", {resultado})
